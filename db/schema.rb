a73a58e89a30e0fe8ac5dd9c0185da3bd16f3808
hiroyuki.inuyama <kaigaigame34@gmail.com>
2019-09-20 20:02:27 +0000

    add db-seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_200701) do
+ActiveRecord::Schema.define(version: 2019_09_20_181000) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
